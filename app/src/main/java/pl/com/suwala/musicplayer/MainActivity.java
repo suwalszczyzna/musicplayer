e08d7564204068bc651e927ee27cddfc93085432
Damian <damian.suwala@gmail.com>
2018-02-24 20:19:39 +0100

    Navigation drawer

--- app/src/main/java/pl/com/suwala/musicplayer/MainActivity.java
+++ app/src/main/java/pl/com/suwala/musicplayer/MainActivity.java
@@ -3,15 +3,22 @@ package pl.com.suwala.musicplayer;
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         setContentView(R.layout.activity_main);
         ViewPager viewPager = findViewById(R.id.viewpager);
@@ -22,10 +29,24 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
+        mDrawerLayout = findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
 
+        return super.onOptionsItemSelected(item);
+    }
 }
